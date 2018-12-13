c7d39508f70028a09137ab275a0d5760e9bc4c9d
Alex Kerney <abk@mac.com>
2018-12-13 13:35:32 -0500

    Add time series model tests

--- app/deployments/tests/test_models.py
+++ app/deployments/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from deployments.models import Deployment, Program, Platform, ProgramAttribution, StationType, MooringType, DataType, BufferType, ErddapServer
+from deployments.models import Deployment, Program, Platform, ProgramAttribution, StationType, MooringType, DataType, BufferType, ErddapServer, TimeSeries
 
 
 class ProgramTestCase(TestCase):
@@ -235,3 +235,102 @@ class ErddapServerTestCase(TestCase):
         from erddapy import ERDDAP
 
         self.assertIsInstance(self.server_with_name.connection(), ERDDAP)
+
+
+class TimeSeriesTestCase(TestCase):
+    fixtures = ['platforms', 'erddapservers']
+
+    def setUp(self):
+        self.platform = Platform.objects.get(name='N01')
+        self.erddap = ErddapServer.objects.get(base_url='http://www.neracoos.org/erddap')
+
+        self.conductivity = DataType.objects.get(standard_name='sea_water_electrical_conductivity')
+        self.water_temp = DataType.objects.get(standard_name='sea_water_temperature')
+        self.current_direction = DataType.objects.get(standard_name='direction_of_sea_water_velocity')
+
+        self.buffer_type = BufferType.objects.get(name='sbe37')
+
+        # two time series from the same dataset and constraints
+        self.ts1 = TimeSeries.objects.create(
+            platform=self.platform,
+            data_type=self.conductivity,
+            variable='conductivity',
+            constraints={"depth=": 1.0},
+            depth=1,
+            start_time='2004-06-03 21:00:00+00',
+            end_time=None,
+            buffer_type=self.buffer_type,
+            erddap_dataset='N01_sbe37_all',
+            erddap_server=self.erddap
+        )
+        self.ts2 = TimeSeries.objects.create(
+            platform=self.platform,
+            data_type=self.water_temp,
+            variable='temperature',
+            constraints={"depth=": 1.0},
+            depth=1,
+            start_time='2004-06-03 21:00:00+00',
+            end_time=None,
+            buffer_type=self.buffer_type,
+            erddap_dataset='N01_sbe37_all',
+            erddap_server=self.erddap
+        )
+
+        # one with the same dataset but a different constraint
+        self.ts3 = TimeSeries.objects.create(
+            platform=self.platform,
+            data_type=self.water_temp,
+            variable='temperature',
+            constraints={"depth=": 100.0},
+            depth=1,
+            start_time='2004-06-03 21:00:00+00',
+            end_time=None,
+            buffer_type=self.buffer_type,
+            erddap_dataset='N01_sbe37_all',
+            erddap_server=self.erddap
+        )
+
+        # one with the same constraint but a different dataset
+        self.ts4 = TimeSeries.objects.create(
+            platform=self.platform,
+            data_type=self.current_direction,
+            variable='current_direction',
+            constraints={"depth=": 100.0},
+            depth=1,
+            start_time='2004-06-03 21:00:00+00',
+            end_time=None,
+            buffer_type=None,
+            erddap_dataset='N01_aanderaa_all',
+            erddap_server=self.erddap
+        )
+
+        # one that has an end_time so it should not be offered
+        self.ts5 = TimeSeries.objects.create(
+            platform=self.platform,
+            data_type=self.water_temp,
+            variable='temperature',
+            constraints={"depth=": 100.0},
+            depth=1,
+            start_time='2004-06-03 21:00:00+00',
+            end_time='2007-06-03 21:00:00+00',
+            buffer_type=self.buffer_type,
+            erddap_dataset='N01_sbe37_all',
+            erddap_server=self.erddap
+        )
+
+    def test_timeseries_str(self):
+        self.assertIn('N01', str(self.ts1))
+        self.assertIn('sea_water_electrical_conductivity', str(self.ts1))
+        self.assertIn('1', str(self.ts1))
+
+    def test_grouped_timeseries(self):
+        group = self.platform.group_timeseries_by_erddap_dataset()
+
+        self.assertEquals(3, len(group))
+
+        time_series = []
+
+        for values in group.values():
+            time_series += values
+        
+        self.assertEquals(4, len(time_series))

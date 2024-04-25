8c6356c46d97200af34ce22b4aeca178db2e56ff
Alex Kerney <abk@mac.com>
2024-04-25 11:06:00 -0400

    Post-pre-commit

--- app/deployments/migrations/0051_timeseries_highlighted.py
+++ app/deployments/migrations/0051_timeseries_highlighted.py
@@ -4,15 +4,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('deployments', '0050_alter_platform_station_name'),
+        ("deployments", "0050_alter_platform_station_name"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='timeseries',
-            name='highlighted',
-            field=models.CharField(choices=[('No', 'No'), ('Before', 'Before'), ('After', 'After')], default='No', help_text='Should this timeseries be elevated to current conditions/latest values, and should it go before or after the existing set?', max_length=50),
+            model_name="timeseries",
+            name="highlighted",
+            field=models.CharField(
+                choices=[("No", "No"), ("Before", "Before"), ("After", "After")],
+                default="No",
+                help_text="Should this timeseries be elevated to current conditions/latest values, and should it go before or after the existing set?",
+                max_length=50,
+            ),
         ),
     ]

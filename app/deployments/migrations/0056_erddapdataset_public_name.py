0fed68237401d5c47ee05572e58f84c5a26fb25a
Alex Kerney <abk@mac.com>
2024-12-04 07:59:38 -0500

    Post-pre-commit

--- app/deployments/migrations/0056_erddapdataset_public_name.py
+++ app/deployments/migrations/0056_erddapdataset_public_name.py
@@ -4,15 +4,19 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('deployments', '0055_timeseries_extrema_values'),
+        ("deployments", "0055_timeseries_extrema_values"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='erddapdataset',
-            name='public_name',
-            field=models.CharField(blank=True, help_text='The name of the dataset as it should be displayed in the UI', max_length=256, null=True),
+            model_name="erddapdataset",
+            name="public_name",
+            field=models.CharField(
+                blank=True,
+                help_text="The name of the dataset as it should be displayed in the UI",
+                max_length=256,
+                null=True,
+            ),
         ),
     ]

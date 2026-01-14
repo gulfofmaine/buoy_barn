3f297fd4f17fc80796939287f85e94747e45f610
Alex Kerney <abk@mac.com>
2026-01-14 18:04:12 -0500

    MQTT subscription support
    For the fastest refreshing of datasets, Buoy Barn can subscribe to the [MQTT](https://erddap.github.io/docs/server-admin/mqtt-integration#use-case-2-publishing-dataset-change-notifications) service for an ERDDAP server.
    
      - For this, add the MQTT broker, port, user, and password to the ERDDAP server configuration in the admin.
    
      - Then run `manage.py erddap_mqtt <server_name>`. This will connect to the server, subscribe to `change/#` to get all dataset updates, then for every message check the topic `change/<dataset_id>` and trigger a refresh of that dataset in the background.
      
      - Not all messages to a dataset topic are necessarily new data, but they generally mean a change to how ERDDAP understands a dataset, so it's worth attempting a refresh. Because this can be a bit noisy, it checks that there isn't another refresh already scheduled to manage the load on the ERDDAP server.
    
    This may require running `docker network create erddap_mqtt_network` before Docker Compose can bring up the MQTT service (or all?).
    
    
    Closes #1453

---
+++ app/deployments/management/commands/erddap_mqtt.py
@@ -0,0 +1,60 @@
+import paho.mqtt.client as mqtt
+from django.core.management.base import BaseCommand, CommandError, CommandParser
+
+from deployments.models import ErddapDataset, ErddapServer
+from deployments.tasks import single_refresh_dataset
+
+
+class Command(BaseCommand):
+    help = "Start the ERDDAP MQTT subscriber"
+
+    def add_arguments(self, parser: CommandParser) -> None:
+        parser.add_argument(
+            "server_slug",
+            help="The slug/name of the ERDDAP server to subscribe to",
+            type=str,
+        )
+
+    def handle(self, *args, **options):
+        server_slug = options["server_slug"]
+
+        erddap_server = ErddapServer.objects.get(name=server_slug)
+
+        if not erddap_server.mqtt_broker:
+            raise CommandError(f"No MQTT broker configured for ERDDAP server {erddap_server}")
+
+        if not erddap_server.mqtt_username or not erddap_server.mqtt_password:
+            raise CommandError(f"No MQTT credentials configured for ERDDAP server {erddap_server}")
+
+        self.stdout.write(f"Starting ERDDAP MQTT subscriber for {erddap_server}")
+
+        def on_connect(client, userdata, flags, reason_code, properties):
+            self.stdout.write(
+                f"Connected to MQTT broker ({erddap_server.mqtt_broker}) for {erddap_server}",
+            )
+            client.subscribe("change/#")
+            self.stdout.write(self.style.SUCCESS(f"Subscribed to change/# topics for {erddap_server}"))
+
+        def on_message(client, userdata, msg):
+            dataset_name = msg.topic.split("/")[-1]
+            self.stdout.write(f"ERDDAP dataset updated: {dataset_name}")
+            try:
+                dataset = ErddapDataset.objects.get(name=dataset_name, server=erddap_server)
+            except ErddapDataset.DoesNotExist:
+                self.stdout.write(
+                    self.style.NOTICE(
+                        f"No dataset found for name {dataset_name} on server {erddap_server}",
+                    ),
+                )
+                return
+            single_refresh_dataset.delay(dataset.id)
+            self.stdout.write(self.style.SUCCESS(f"Scheduled refresh for dataset {dataset}"))
+
+        mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        mqttc.on_connect = on_connect
+        mqttc.on_message = on_message
+
+        mqttc.username_pw_set(username=erddap_server.mqtt_username, password=erddap_server.mqtt_password)
+        mqttc.connect(erddap_server.mqtt_broker, 1883, 60)
+
+        mqttc.loop_forever()
